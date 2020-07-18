f72d4b7e8b2d41d018713b9087348c8fe78dc0ad
Hirdeshgupta <hirdeshgupta16@gmail.com>
2020-07-18 18:50:44 +0530

    My Site is Ready for Deployment

---
+++ assets/js/main.js
@@ -0,0 +1,38 @@
+$(window).on('load', function() {
+
+    $('.level-bar-inner').each(function() {
+
+        var itemWidth = $(this).data('level');
+
+        $(this).animate({
+            width: itemWidth
+        }, 800);
+
+    });
+
+});
+
+
+jQuery(document).ready(function($) {
+
+
+    /*======= Skillset *=======*/
+
+    $('.level-bar-inner').css('width', '0');
+
+
+
+    /* Bootstrap Tooltip for Skillset */
+    $('.level-label').tooltip();
+
+
+
+    /* Github Calendar - https://github.com/IonicaBizau/github-calendar */
+    new GitHubCalendar("#github-graph", "Hirdeshgupta");
+
+
+    /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
+    GitHubActivity.feed({ username: "Hirdeshgupta", selector: "#ghfeed" });
+
+
+});
