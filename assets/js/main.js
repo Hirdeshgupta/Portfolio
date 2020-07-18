6cba8a1b0faf0f7e88bbfff0e4ab43acbbb5a804
Hirdeshgupta <hirdeshgupta16@gmail.com>
2020-07-18 19:09:49 +0530

    Mobile view updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,7 +28,7 @@ jQuery(document).ready(function($) {
 
 
     /* Github Calendar - https://github.com/IonicaBizau/github-calendar */
-    new GitHubCalendar("#github-graph", "Hirdeshgupta");
+    new GitHubCalendar("#github-graph", "Hirdeshgupta", { responsive: true });
 
 
     /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
